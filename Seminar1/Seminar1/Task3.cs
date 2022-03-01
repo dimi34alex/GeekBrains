2a12054bdfa25bd503485dad5614c523bd41fe6d
dimi34alex <dimi_alex@mail.ru>
2022-03-01 19:20:03 +0300

    123

---
+++ Seminar1/Seminar1/Task3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar1
+{
+    class Task3
+    {
+        static double Rass(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+        static void Main()
+        {
+            Console.WriteLine("Введите координаты двух точек:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0}", Rass(x1, y1, x2, y2));
+        }
+    }
+}
---
+++ Seminar1/Seminar1/Task5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar1
+{
+    class Task5
+    {
+        static void print()
+        {
+            Console.WriteLine("\tАлексеенко \n\tДмитрий\n\tВолгоград");
+        }
+        static void Main()
+        {
+            print();
+        }
+    }
+}
