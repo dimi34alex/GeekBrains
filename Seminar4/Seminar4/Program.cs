ee14c6acbda029b1a4831674848feaf4ae4a4153
dimi34alex <dimi_alex@mail.ru>
2022-03-31 19:40:11 +0300

    Seminar 4

---
+++ Seminar4/Seminar4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int count = 0;
+            int[] a = { };
+            a = new int[20];
+            for (int i = 0; i <20; i++)
+            {
+                a[i] = r.Next(-10000,10000);
+                Console.WriteLine(a[i]);
+            }
+            for (int i = 0; i <= a.Length-2; i++)
+            {
+                if ((a[i] % 3 == 0 && a[i + 1] % 3 != 0) || (a[i] % 3 != 0 && a[i + 1] % 3== 0))
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Seminar4/Seminar4/Task2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Seminar4
+{
+    static class StaticClass
+    {
+        static int Pari(int[] a)
+        {
+            int count = 0;
+            for (int i = 0; i <= a.Length - 2; i++)
+            {
+                if ((a[i] % 3 == 0 && a[i + 1] % 3 != 0) || (a[i] % 3 != 0 && a[i + 1] % 3 == 0))
+                    count++;
+            }
+            return count;
+        }
+        static int[] Fromtxt(string path, int n)
+        {
+            int[] error = new int[0];
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                int[] ar = new int[n];
+                for (int i = 0; i < ar.Length; i++)
+                {
+                    ar[i] = int.Parse(sr.ReadLine());
+                }
+                return ar;
+            }
+            catch
+            {
+                Console.WriteLine("Не найден файл на диске!");
+                return error;
+            }
+        }
+    }
+    class Task2
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
